08a19640032bd66501716ac271b411a049f971ec
= <=>
2025-04-27 21:43:53 +0545

    [UPT]: Updated movie seeder to use static data

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -4,82 +4,92 @@ namespace Database\Seeders;
 
 use App\Models\Movie;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class MovieSeeder extends Seeder
 {
-    protected $apiKey;
-    protected $baseUrl = 'https://api.themoviedb.org/3';
-
-    public function __construct()
-    {
-        $this->apiKey = env('TMDB_API_KEY');
-    }
-
     public function run(): void
     {
-        if (empty($this->apiKey)) {
-            Log::error('TMDB API key is not set in .env file');
-            return;
+        $movies = [
+            [
+                'title' => 'The Dark Knight',
+                'description' => 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+                'duration' => 152,
+                'release_date' => '2008-07-18',
+                'poster_url' => 'https://i1.sndcdn.com/artworks-000480914043-0a2fis-t500x500.jpg',
+                'director' => 'Christopher Nolan',
+                'genre' => ['Action', 'Crime', 'Drama'],
+                'rating' => 9.0,
+                'language' => 'English',
+                'country' => 'United States',
+            ],
+            [
+                'title' => 'Inception',
+                'description' => 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+                'duration' => 148,
+                'release_date' => '2010-07-16',
+                'poster_url' => 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
+                'director' => 'Christopher Nolan',
+                'genre' => ['Action', 'Sci-Fi', 'Thriller'],
+                'rating' => 8.8,
+                'language' => 'English',
+                'country' => 'United States',
+            ],
+            [
+                'title' => 'The Shawshank Redemption',
+                'description' => 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+                'duration' => 142,
+                'release_date' => '1994-09-23',
+                'poster_url' => 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+                'director' => 'Frank Darabont',
+                'genre' => ['Drama'],
+                'rating' => 9.3,
+                'language' => 'English',
+                'country' => 'United States',
+            ],
+            [
+                'title' => 'The Godfather',
+                'description' => 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+                'duration' => 175,
+                'release_date' => '1972-03-14',
+                'poster_url' => 'https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+                'director' => 'Francis Ford Coppola',
+                'genre' => ['Crime', 'Drama'],
+                'rating' => 9.2,
+                'language' => 'English',
+                'country' => 'United States',
+            ],
+            [
+                'title' => 'Pulp Fiction',
+                'description' => 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+                'duration' => 154,
+                'release_date' => '1994-09-10',
+                'poster_url' => 'https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg',
+                'director' => 'Quentin Tarantino',
+                'genre' => ['Crime', 'Drama'],
+                'rating' => 8.9,
+                'language' => 'English',
+                'country' => 'United States',
+            ],
+        ];
+
+        foreach ($movies as $movie) {
+            Movie::firstOrCreate(
+                ['title' => $movie['title']],
+                [
+                    'title' => $movie['title'],
+                    'description' => $movie['description'],
+                    'duration' => $movie['duration'],
+                    'release_date' => $movie['release_date'],
+                    'poster_url' => $movie['poster_url'],
+                    'director' => $movie['director'],
+                    'genre' => $movie['genre'],
+                    'rating' => $movie['rating'],
+                    'language' => $movie['language'],
+                    'country' => $movie['country'],
+                ]
+            );
         }
 
-        // Fetch popular movies
-        $response = Http::withOptions([
-            'verify' => false
-        ])->get("{$this->baseUrl}/movie/popular", [
-            'api_key' => $this->apiKey,
-            'language' => 'en-US',
-            'page' => 1,
-        ]);
-
-        if ($response->successful()) {
-            $movies = $response->json()['results'];
-
-            foreach ($movies as $movieData) {
-                // Fetch movie details to get additional information
-                $movieDetails = Http::withOptions([
-                    'verify' => false
-                ])->get("{$this->baseUrl}/movie/{$movieData['id']}", [
-                    'api_key' => $this->apiKey,
-                    'language' => 'en-US',
-                    'append_to_response' => 'credits',
-                ])->json();
-
-                // Get director from credits
-                $director = collect($movieDetails['credits']['crew'] ?? [])
-                    ->where('job', 'Director')
-                    ->first()['name'] ?? 'Unknown';
-
-                // Get genres
-                $genres = collect($movieDetails['genres'] ?? [])
-                    ->pluck('name')
-                    ->toArray();
-
-                Movie::firstOrCreate(
-                    ['title' => $movieData['title']],
-                    [
-                        'title' => $movieData['title'],
-                        'description' => $movieData['overview'],
-                        'duration' => $movieDetails['runtime'] ?? 120,
-                        'release_date' => $movieData['release_date'],
-                        'poster_url' => "https://image.tmdb.org/t/p/w500{$movieData['poster_path']}",
-                        'director' => $director,
-                        'genre' => $genres ?: 'Unknown',
-                        'rating' => $movieData['vote_average'] ?? null,
-                        'language' => $movieDetails['original_language'] ?? 'English',
-                        'country' => $movieDetails['production_countries'][0]['name'] ?? null,
-                    ]
-                );
-            }
-
-            $this->command->info('Successfully seeded movies from TMDB API');
-        } else {
-            Log::error('Failed to fetch movies from TMDB API', [
-                'status' => $response->status(),
-                'body' => $response->body(),
-            ]);
-            $this->command->error('Failed to fetch movies from TMDB API');
-        }
+        $this->command->info('Successfully seeded sample movies');
     }
 }
