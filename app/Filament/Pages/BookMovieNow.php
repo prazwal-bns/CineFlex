60bde95de28a7a3057dc4974afe74522cfbb52d0
prazwal-bns <prajwalbns15@gmail.com>
2025-06-06 00:57:29 +0545

    Refactor BookMovieNow page to streamline movie loading, enhance UI elements, and improve styling for better user experience

--- app/Filament/Pages/BookMovieNow.php
+++ app/Filament/Pages/BookMovieNow.php
@@ -2,41 +2,21 @@
 
 namespace App\Filament\Pages;
 
-use Filament\Pages\Page;
 use App\Models\Movie;
-use App\Models\Showtime;
-use Illuminate\Support\Collection;
+use Filament\Pages\Page;
 
 class BookMovieNow extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-film';
-    protected static ?string $navigationLabel = 'Book Movies';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
     protected static string $view = 'filament.pages.book-movie-now';
 
-    public Collection $movies;
+    protected static ?string $title = 'Book a Movie';
+
+    public $movies;
 
     public function mount(): void
     {
-        $this->loadMovies();
-    }
-
-    public function loadMovies(): void
-    {
-        $this->movies = Movie::with(['showtimes' => function ($query) {
-            $query->where('start_time', '>', now())
-                  ->orderBy('start_time', 'asc');
-        }])
-        ->whereHas('showtimes', function ($query) {
-            $query->where('start_time', '>', now());
-        })
-        ->get();
-    }
-
-    public function selectShowtime($movieId, $showtimeId)
-    {
-        return redirect()->route('filament.admin.pages.select-showtime', [
-            'movie' => $movieId,
-            'showtime' => $showtimeId
-        ]);
+        $this->movies = Movie::get();
     }
 }
