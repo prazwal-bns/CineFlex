83f074c0ada056ee437866a681a5bd1aca5f3ada
prazwal-bns <prajwalbns15@gmail.com>
2025-10-06 11:35:36 +0545

    Update AdminPanelProvider to use Action for profile menu item, enhancing consistency with Filament actions.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use App\Filament\Pages\Profile;
 use App\Filament\Widgets\CustomAccountWidget;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use Filament\Actions\Action;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -34,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->spa()
             ->userMenuItems([
-                MenuItem::make()
+                Action::make('profile')
                     ->label('Profile')
                     ->url(fn (): string => Profile::getUrl())
                     ->icon('heroicon-o-user'),
