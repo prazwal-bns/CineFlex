12ac82dd0152b2ca09f67dfabde5fae58493990a
prazwal-bns <prajwalbns15@gmail.com>
2025-11-18 20:13:23 +0545

    Add notification for payment status updates in PaymentResourceTable

--- app/Filament/Resources/PaymentResource/Pages/Tables/PaymentResourceTable.php
+++ app/Filament/Resources/PaymentResource/Pages/Tables/PaymentResourceTable.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\PaymentResource\Pages\Tables;
 
 use App\Enums\PaymentStatus;
 use App\Filament\Contracts\ResourceFieldContract;
+use Filament\Notifications\Notification;
 use Filament\Tables;
 
 final class PaymentResourceTable implements ResourceFieldContract
@@ -31,6 +32,12 @@ final class PaymentResourceTable implements ResourceFieldContract
                 ->afterStateUpdated(function ($state, $record) {
                     $record->status = PaymentStatus::from($state);
                     $record->save();
+
+                    Notification::make()
+                        ->title('Payment Status Updated')
+                        ->body('The payment status has been updated to ' . $record->status->getLabels())
+                        ->success()
+                        ->send();
                 }),
             Tables\Columns\TextColumn::make('created_at')
                 ->dateTime()
